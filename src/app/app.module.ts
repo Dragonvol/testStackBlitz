fb924600ae7f8a7a8c947a23fef7a322c969a034
eldawidos1111 <eldawidos111@gmail.com>
2022-11-26 11:34:51 +0100

    test logowanie
    bez api i ról jak na razie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helper';
 
 import { AppComponent } from './app.component';
-import { ClientsComponent } from './clients/clients.component';
+import { AppRoutingModule } from './app.routing-module';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helper';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    ClientsComponent
+    HomeComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
